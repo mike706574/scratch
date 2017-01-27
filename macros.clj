402ed38559617cffa4a6442803cf652b2cc4ba15
mike <mike706574@gmail.com>
2017-01-26 22:41:04 -0600

    Initial.

---
+++ macros.clj
@@ -0,0 +1,128 @@
+(defmacro defp
+  [sym v]
+  `(do (def ~sym ~v)
+       (def ~(build-symbol sym "-plus") ~(inc v))))
+
+(defmacro deft
+  [fn-symbol args & body]
+  `(def ~fn-symbol
+     (fn ~args
+       (go
+         (try
+           (if (nil? (second ~args))
+             {:status :bad-request :body "Type was nil!"}
+             (if (type-exists? (first ~args) (second ~args))
+               ~(conj body `do)
+               {:status :missing :body (str "Invalid type ID: " (second ~args))}))
+           (catch Exception e# {:status :error :body (.getMessage e#) :exception e#}))))))
+
+(defmacro defm
+  [fn-symbol & body]
+  `(defn ~fn-symbol [id#]
+     (pprint
+      (let [~'repo (artifact/repo config)
+            ~'id (name id#)]
+        ~@body))))
+
+(defp what 4)
+
+(defn build-symbol
+  [& args]
+  (symbol (apply str (map name args))))
+
+
+(defmacro defmonitor
+  [sym]
+  `(do
+     (defonce ~sym (atom {}))
+     (defn ~(build-symbol "monitor-" sym "!") [id#] (swap! ~sym add-monitor id#) nil)
+     (defn ~(build-symbol "terminate-" sym "!") [id#] (swap! ~sym remove-monitor id#) nil)
+     (defn ~(build-symbol "clear-" sym "!") [] (swap! ~sym clear-monitors) nil)
+     (defn ~(build-symbol "active-" sym) [] (keys (deref ~sym)))))
+
+
+(defmacro explode [& strings]
+  `(throw (RuntimeException. ~(apply str strings))))
+
+;; madness
+(cons 4 [1 2 3])
+
+(defmacro defjam
+  [fn-symbol args & body]
+  `(defn ~fn-symbol ~(into [] (cons ~id args))
+     (let [~'gitlab-id (get-gitlab-id id)]
+       ~@body)))
+
+(macroexpand
+ (defjam hi-mom
+  [yo-dawg]
+  (println id)
+  (println gitlab-id)
+  (println yo-dawg)))
+
+(defmacro pull [n m] `(def n (get ~m ~(keyword n))))
+
+(defmacro validate
+    [& body]
+    {:pre [(odd? (count body))]}
+    (let [pairs (partition 2 body)
+                  default (last body)]
+          (concat
+                 [`cond]
+                 (apply concat
+                        (for [[condition message] pairs]
+                          [condition {:status :bad :message message}]))
+                 [:else default])))
+
+
+(defmacro defaliases
+  [& pairs]
+  (let [args (gensym 'args)]
+    `(do
+       ~@(for [[action-name f] (partition 2 pairs)]
+           `(defmethod run ~action-name
+              [~(symbol "action") & ~args]
+              (apply ~f ~args))))))
+
+  (defmacro cfn
+    [arg-list & body]
+    `(with-meta
+       (fn ~arg-list
+         ~(conj body `do))
+       {:args ~(mapv name arg-list)}))
+
+  (defmacro defcmd
+    [symbol arg-list & body]
+    `(def ~symbol
+       (with-meta
+         (fn ~arg-list
+           ~(conj body `do))
+         {:args ~(mapv name (rest arg-list))})))
+
+
+(defmacro assoc-when
+  [test map & kvs]
+  `(if ~test
+     (apply assoc ~map ~(vec kvs))
+     ~map))
+
+(defmacro when-ok
+    [response & fn-body]
+  `(when (= (:status ~response) :ok)
+     (let [~'body (:body ~response)]
+        ~(conj fn-body `do))
+      (throw (ex-info status ~response))))
+
+ (when-ok {:status :foo}
+   (println body))
+
+
+(defn ns-key [ns k] (keyword (name ns) (name k)))
+
+(def ns-key-here (partial ns-key (.getName *ns*)))
+
+(defn walk-keys
+  [f m]
+  (letfn [(on-entry [[k v]]
+            [(f k) v])]f
+         (clojure.walk/postwalk (fn [x] (if (map? x) (into {} (map on-entry x)) x)) m)))
