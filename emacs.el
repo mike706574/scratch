f2e912b173518d2105ff05980abd5bce4d1120e8
mike <mike706574@users.noreply.github.com>
2017-03-23 11:37:52 -0500

    Update emacs.el

--- emacs.el
+++ emacs.el
@@ -5,4 +5,4 @@
       (select-window (active-minibuffer-window))
     (error "Minibuffer is not active")))
 
-(global-set-key "\C-co" 'switch-to-minibuffer) ;; Bind to `C-c o'
+(global-set-key "\C-cm" 'switch-to-minibuffer) ;; Bind to `C-c o'
